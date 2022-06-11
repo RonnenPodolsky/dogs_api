9dc47679b66e4cc27b7c148f87986a9e68fc6730
ronnenn podolsky <ronnennpodolsky@ronnenns-MacBook-Pro.local>
2022-06-11 03:07:09 +0300

    fixed

--- script.js
+++ script.js
@@ -4,30 +4,28 @@ const inputSelector = document.querySelector("#dog-input");
 const allBreeds = document.querySelector("#dog-breeds");
 const selectBreed = document.querySelector("select")
 
-const getBreeds = () => {
-    fetch('https://dog.ceo/api/breeds/list/all')
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      for (element in data.message){
-          allBreeds.innerHTML += `<option value="${element}">${element}</option>`
-      }
-  });
+const updateSelectBreed = async (breeds) => {
+    console.log(breeds)
+    for (const breed of breeds){
+        allBreeds.innerHTML += `<option value="${breed}">${breed}</option>`
+    }
 }
 
-const getDog = url => {
-    fetch(url)
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        imgSelector.src = data.message;
-        return data
-      })
+const getBreeds = async (url) => {
+    const response = await fetch(url)
+    const data = await response.json()
+    return Object.keys(data.message)
 }
 
-getBreeds()
+const getDog = async url => {
+    const response = await fetch(url)
+    const data = await response.json();
+    imgSelector.src = data.message;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    getBreeds('https://dog.ceo/api/breeds/list/all').then(breeds => updateSelectBreed(breeds))
+})
 
 selectBreed.addEventListener('change', e => {
     e.preventDefault();
@@ -35,8 +33,6 @@ selectBreed.addEventListener('change', e => {
     const url = `https://dog.ceo/api/breed/${selectDog}/images/random`;
     getDog(url)
     inputSelector.value = ""
-
-
 })
 
 btnSelector.addEventListener('click', e => {
